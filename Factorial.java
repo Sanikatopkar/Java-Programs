8e88fe219f4db4fe7c2b323a8e3374735f3e6e09
Sanikatopkar <sanikatopkar026@gmail.com>
2026-01-13 19:06:57 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,12 @@
+public class Factorial {
+ public static void main(String[] args)
+ {
+   int no=5, long factorial=1;
+   
+  for(int i=1;i<=no;++i;){
+  factorial=factorial*i;
+ }
+System.out.println("The factorial of %d = %d",no,factorial);
+ } 
+}
+
---
+++ Swap.java
@@ -0,0 +1,17 @@
+public class Swap {
+  public static void main(String[] args){
+     int a=20,b=30;
+System.out.println("Before swapping: ");
+System.out.println("First Number: "+ a);
+System.out.println("Second number: "+ b);
+
+int x=a;
+ a=b;
+ b=x;
+
+System.out.println("After swapping: ");
+System.out.println("First number: "+ a);
+System.out.println("Second number: "+ b);
+
+ }
+}
